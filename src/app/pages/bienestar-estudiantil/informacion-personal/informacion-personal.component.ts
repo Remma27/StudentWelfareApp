7dff168fa51e874caeba708c31153b93d429f5d6
Remma27 <emmanuelrsolano27@gmail.com>
2023-08-20 18:16:11 -0600

    Aceptando merge

--- src/app/pages/bienestar-estudiantil/informacion-personal/informacion-personal.component.ts
+++ src/app/pages/bienestar-estudiantil/informacion-personal/informacion-personal.component.ts
@@ -5,72 +5,29 @@ import { ToastrService } from 'ngx-toastr';
 import { EstudiantesForm } from 'src/app/shared/formsModels/estudiantesForms';
 import { EstudiantesService } from 'src/app/shared/services/estudiantes.service';
 
-<<<<<<< Updated upstream
-
-
-interface informacion {
-=======
 interface bitacora {
->>>>>>> Stashed changes
   value: string;
   viewValue: string;
 }
 
-<<<<<<< Updated upstream
-interface  nacimiento {
-  value: string;
-  viewValue: string;
-} 
-
-=======
->>>>>>> Stashed changes
 @Component({
   selector: 'app-informacion-personal',
   templateUrl: './informacion-personal.component.html',
   styleUrls: ['./informacion-personal.component.scss'],
 })
 export class InformacionPersonalComponent {
-<<<<<<< Updated upstream
-  
- 
-
-  informacion: informacion[] = [
-    {value: '1', viewValue: 'I Cuatrimestre'},
-    {value: '2', viewValue: 'II Cuatrimestre'},
-    {value: '3', viewValue: 'III Cuatrimestre'},
-  ];
-
-  nacimiento: nacimiento[] = [
-    {value: '1', viewValue: 'Costarricense'},
-    {value: '2', viewValue: 'Extranjero'},
-  
-  ];
-
-  
-=======
   bitacora: bitacora[] = [
-    {value: '1', viewValue: 'I cuatrimestre'},
-    {value: '2', viewValue: 'II cuatrimestre'},
-    {value: '3', viewValue: 'III cuatrimestre'},
+    { value: '1', viewValue: 'I cuatrimestre' },
+    { value: '2', viewValue: 'II cuatrimestre' },
+    { value: '3', viewValue: 'III cuatrimestre' },
   ];
   titulo = 'Informacion Personal';
   isCreate: boolean = true;
->>>>>>> Stashed changes
   constructor(
     public estudianteForm: EstudiantesForm,
     private srvEstudiante: EstudiantesService,
     private mensajeria: ToastrService
-<<<<<<< Updated upstream
   ) {}
-  
- 
- 
-
-
-=======
-  ) { }
-
-
 
   guardar() {
     this.srvEstudiante.insert(this.estudianteForm.baseForm.value).subscribe(
@@ -86,7 +43,4 @@ export class InformacionPersonalComponent {
       }
     );
   }
->>>>>>> Stashed changes
 }
-
-
